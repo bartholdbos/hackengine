d86c4f7c5b037b74336250b8fe82ea0ad5bbc395
Barthold Bos <bartholdbos@hotmail.com>
2018-02-08 19:19:24 +0100

    Read maps file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <regex>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc != 2){
+        exit(1);
+    }
+
+    std::string pid = argv[1];
+    std::ifstream maps;
+    maps.open("/proc/" + pid + "/maps");
+
+    std::regex values("(\\S+)");
+    std::string line;
+    while(getline(maps, line)){
+        std::smatch value;
+        while(regex_search(line, value, values)){
+            std::cout << value.str() << " ";
+            std::cout << std::endl;
+            line = value.suffix().str();
+        }
+
+    }
+
     return 0;
 }
